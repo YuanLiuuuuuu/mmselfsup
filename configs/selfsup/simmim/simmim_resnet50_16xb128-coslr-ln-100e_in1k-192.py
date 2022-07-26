9d84b12378bda994000471210164c2f886c7cb1e
YuanLiuuuuuu <3463423099@qq.com>
2022-07-26 19:02:34 +0800

    [Fix]: Use layer norm 2d

--- configs/selfsup/simmim/simmim_resnet50_16xb128-coslr-ln-100e_in1k-192.py
+++ configs/selfsup/simmim/simmim_resnet50_16xb128-coslr-ln-100e_in1k-192.py
@@ -1,4 +1,4 @@
 _base_ = 'simmim_resnet50_16xb128-coslr-100e_in1k-192.py'
 
 # model settings
-model = dict(backbone=dict(norm_cfg=dict(type='LN', eps=1e-6)))
+model = dict(backbone=dict(norm_cfg=dict(type='LN2d', eps=1e-6)))
