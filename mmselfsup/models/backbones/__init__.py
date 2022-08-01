fe17e9323e0da2aa371f65141f4f3524391503e9
YuanLiuuuuuu <3463423099@qq.com>
2022-08-01 14:40:17 +0800

    [Fix]: Fix simmim resnet

--- mmselfsup/models/backbones/__init__.py
+++ mmselfsup/models/backbones/__init__.py
@@ -6,8 +6,9 @@ from .resnet import ResNet, ResNetSobel, ResNetV1d
 from .resnext import ResNeXt
 from .simmim_swin import SimMIMSwinTransformer
 from .simmim_convnext import SimMIMConvNext
+from .simmim_resnet import SimMIMResNet
 
 __all__ = [
     'ResNet', 'ResNetSobel', 'ResNetV1d', 'ResNeXt', 'MAEViT', 'MoCoV3ViT',
-    'SimMIMSwinTransformer', 'CAEViT', 'SimMIMConvNext'
+    'SimMIMSwinTransformer', 'CAEViT', 'SimMIMConvNext', 'SimMIMResNet'
 ]
