d2767b31afb6d9af0993b74ec2d065a9b788b980
YuanLiuuuuuu <3463423099@qq.com>
2022-10-31 14:21:26 +0800

    [Feature]: Ft 30e

---
+++ configs/benchmarks/classification/imagenet/vit-large-p16_ft-8xb128-coslr-30e_in1k-448.py
@@ -0,0 +1,20 @@
+_base_ = 'vit-large-p16_ft-8xb128-coslr-50e_in1k-448.py'
+
+param_scheduler = [
+    dict(
+        type='LinearLR',
+        start_factor=0.0001,
+        by_epoch=True,
+        begin=0,
+        end=5,
+        convert_to_iter_based=True),
+    dict(
+        type='CosineAnnealingLR',
+        T_max=25,
+        by_epoch=True,
+        begin=5,
+        end=30,
+        eta_min=1e-06,
+        convert_to_iter_based=True)
+]
+train_cfg = dict(by_epoch=True, max_epochs=30, val_interval=1)
