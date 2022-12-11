ef4bfdfa962ac42ce5a665011786ace89b028553
YuanLiuuuuuu <3463423099@qq.com>
2022-12-11 10:10:37 +0800

    [Fix]: Set the embed_dim to 1024 for large

--- configs/selfsup/mae_plus/mae-plus_vit-large-p16_8xb512-amp-coslr-1600e_in1k.py
+++ configs/selfsup/mae_plus/mae-plus_vit-large-p16_8xb512-amp-coslr-1600e_in1k.py
@@ -1,4 +1,4 @@
 _base_ = 'mae-plus_vit-base-p16_8xb512-amp-coslr-1600e_in1k.py'
 
 # model settings
-model = dict(backbone=dict(arch='l'))
+model = dict(backbone=dict(arch='l'), neck=dict(embed_dim=1024))
