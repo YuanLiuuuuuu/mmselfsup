ec1c6c863ce7fe16b0105fa451b70fb431edbf5c
liuyuan <3463423099@qq.com>
2023-02-16 15:22:39 +0800

    [Fix]: Fix ms tiny

--- configs/selfsup/mae_plus/mae-plus_vit-tiny-p16_8xb512-amp-coslr-300e_in1k-ms.py
+++ configs/selfsup/mae_plus/mae-plus_vit-tiny-p16_8xb512-amp-coslr-300e_in1k-ms.py
@@ -2,5 +2,5 @@ _base_ = 'mae-plus_vit-small-p16_8xb512-amp-coslr-300e_in1k-ms.py'
 
 # model settings
 model = dict(
-    backbone=dict(arch='deit-s', out_indices=[3, 5, 7, 11]),
+    backbone=dict(arch='deit-t', out_indices=[3, 5, 7, 11]),
     neck=dict(embed_dim=192, decoder_embed_dim=192, decoder_num_heads=12))
